aa2e0603e5e4dc1cbc1ff64900897616d048bcaf
mikeo <29011818+mikeo@users.noreply.github.com>
2024-04-13 20:57:31 +0100

    Revert "Update ContentView.swift"
    This reverts commit a19d35c453eeea11f3eb773cc766eb5bde3c10f9.

--- Geranium/ContentView.swift
+++ Geranium/ContentView.swift
@@ -10,6 +10,36 @@ import SwiftUI
 struct ContentView: View {
     @State var defaultTab = AppSettings().defaultTab
     var body: some View {
-        continue
+        TabView(selection: $defaultTab) {
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house.fill")
+                }
+                .tag(1)
+            DaemonView()
+                .tabItem {
+                    Label("Daemons", systemImage: "flag.fill")
+                }
+                .tag(2)
+                .onAppear {
+                    RootHelper.removeItem(at: URL(fileURLWithPath: "/var/mobile/Documents/disabled.plist"))
+                }
+            LocSimView()
+                .tabItem {
+                    Label("LocSim", systemImage: "mappin")
+                }
+                .tag(3)
+            CleanerView()
+                .tabItem {
+                    Label("Cleaner", systemImage: "trash.fill")
+                }
+                .tag(4)
+                .animation(.easeInOut)
+            SuperviseView()
+                .tabItem {
+                    Label("Superviser", systemImage: "checkmark.seal.fill")
+                }
+                .tag(5)
+        }
     }
 }
