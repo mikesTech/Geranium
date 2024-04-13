972433c31673b410e78a0abe4f6507ca4dc33863
mikeo <29011818+mikeo@users.noreply.github.com>
2024-04-13 21:53:48 +0100

    how do i update zoom and not location
    i dont know any swift at all i have never developed ios im googling so many solutions

--- Geranium/LocSim/CustomMapView.swift
+++ Geranium/LocSim/CustomMapView.swift
@@ -17,9 +17,9 @@ struct CustomMapView: UIViewRepresentable {
         mapView.showsUserLocation = true
         mapView.layer.cornerRadius = 15
         mapView.layer.masksToBounds = true
-        let newCamera: MKMapCamera = MKMapCamera(lookingAtCenter: mapView.camera.centerCoordinate, fromEyeCoordinate: mapView.camera.centerCoordinate, eyeAltitude: 10)
+        CLLocationCoordinate2D noLocation
+        let newCamera: MKMapCamera = MKMapCamera(lookingAtCenter: noLocation, fromEyeCoordinate: noLocation, eyeAltitude: 10)
         mapView.setCamera(newCamera, animated: true)
-
         let tapRecognizer = UITapGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.handleTap(_:)))
         mapView.addGestureRecognizer(tapRecognizer)
         return mapView
