71d62d3215ce23cc815bcea30218054eff579b76
mike <54469553+mikesTech@users.noreply.github.com>
2024-04-13 21:26:03 +0100

    Update CustomMapView.swift

--- Geranium/LocSim/CustomMapView.swift
+++ Geranium/LocSim/CustomMapView.swift
@@ -17,7 +17,7 @@ struct CustomMapView: UIViewRepresentable {
         mapView.showsUserLocation = true
         mapView.layer.cornerRadius = 15
         mapView.layer.masksToBounds = true
-        mapView.camera.altitude = 1.05
+        mapView.zoom = 2
 
         let tapRecognizer = UITapGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.handleTap(_:)))
         mapView.addGestureRecognizer(tapRecognizer)
